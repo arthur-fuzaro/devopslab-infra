9debc5162b4fdbfcb736765eae95a498e7a25a9e
Arthur Oliveira <afuzarodeoliveira@gmail.com>
2023-04-19 22:01:41 -0300

    Ajuste de versão

--- providers.tf
+++ providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.54.0"
+      version = "4.62.1"
     }
   }
 }
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  project = var.project_name
+  project = "var.project_name"
 }
